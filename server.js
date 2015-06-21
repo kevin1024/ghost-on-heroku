fdcfecefe91ff2640e97fb8f12f91de3de701af0
Kevin McCarthy <me@kevinmccarthy.org>
2015-06-21 08:43:00 -1000

    add redirects

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-var path = require('path');
-var ghost = require('ghost');
+var ghost = require('ghost'),
+    path = require('path'),
+    express = require('express'),
+    redirects = express();
+
+redirects.all('/blog/*', function(req, res) {
+  var newpath = req.path.replace(/^(\/blog)/,"");
+  res.redirect(301, newpath);
+});
 
 ghost({
   config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
-  ghostServer.start();
+    redirects.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+
+    ghostServer.start(redirects);
 });
